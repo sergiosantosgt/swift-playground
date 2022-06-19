0ea2a0f043e4eaee26bdf05f9499b4571cb90a0a
Sergio Santos <sergiosantos@MacBook-Air-de-Sergio.local>
2022-06-19 20:30:12 -0300

    remove empty spaces

--- SwiftPlayground.playground/Pages/ModelDataStructurevsEnum.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/ModelDataStructurevsEnum.xcplaygroundpage/Contents.swift
@@ -5,25 +5,24 @@ import UIKit
 // Using Enum
 
 enum Session {
-    case keynote(title: String, speaker: String, date: Date, isRecorded: Bool)
-    case normal(title: String, speaker: String, date: Date)
-    case workshop(title: String, speaker: String, date: Date, isRecorded: Bool)
-    case joint(title: String, speakers: [String], date: Date)
+    case keynote(title: String, speaker: String, date: Date, isRecorded: Bool)
+    case normal(title: String, speaker: String, date: Date)
+    case workshop(title: String, speaker: String, date: Date, isRecorded: Bool)
+    case joint(title: String, speakers: [String], date: Date)
 }
 
 let keynote = Session.keynote(title: "WWDC 2022", speaker: "Tim Cook", date: Date(), isRecorded: true)
 
 func displaySession(session: Session) {
-    switch session {
-        case let .keynote(title: title, speaker: speaker, date: date, isRecorded: isRecorded):
-            print("\(title) - \(speaker) - \(date) - \(isRecorded)")
-        case let .normal(title: title, speaker: speaker, date: date):
-            print("\(title) - \(speaker) - \(date)")
-        default:
-            print("")
-    }
-    
-    /*if case let Session.keynote(title: title, speaker: speaker, date: date, isRecorded: isRecorded) = session {
+    switch session {
+        case let .keynote(title: title, speaker: speaker, date: date, isRecorded: isRecorded):
+            print("\(title) - \(speaker) - \(date) - \(isRecorded)")
+        case let .normal(title: title, speaker: speaker, date: date):
+            print("\(title) - \(speaker) - \(date)")
+        default:
+            print("")
+    }
+    /*if case let Session.keynote(title: title, speaker: speaker, date: date, isRecorded: isRecorded) = session {
         // it is a keynote session
     }*/
 }
